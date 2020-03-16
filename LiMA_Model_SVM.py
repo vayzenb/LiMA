d8c706763b4f5c5fb95f6556da92164771037974
vayzenb <vayzenberg90@gmail.com>
2020-03-16 18:08:00 -0400

    final model results

--- LiMA_Model_SVM.py
+++ LiMA_Model_SVM.py
@@ -18,7 +18,7 @@ exp = ['Exp1', 'Exp2']
 
 stim = [['23_Skel', '23_Bulge', '31_Skel', '31_Bulge','26_Skel', '26_Bulge'], \
         ['31_0_Skel', '31_0_Bulge','31_50_Skel', '31_50_Bulge']]
-modelType = ['FF_SN', 'R_SN']
+modelType = ['R_SN']
 
 
 
@@ -31,11 +31,11 @@ folK = 10
 #For single class SVM
 #Nu value is the proportion of outliers you expect (i.e., upper-bound on training data)
 #Gamma parameter determines smoothing of the edges of data (i.e., the )
-clf = svm.OneClassSVM(nu=.01)
+
 
 for ee in range(0,len(exp)):
     n = 0
-    CNN_Acc = np.empty((len(stim[ee]) * len(stim[ee])*len(modelType),8), dtype = object)
+    CNN_Acc = np.empty(((len(stim[ee]) * len(stim[ee])*len(modelType))*10,8), dtype = object)
     
     for mm in range(0, len(modelType)):      
         
@@ -46,55 +46,57 @@ for ee in range(0,len(exp)):
                 trainAcc = 0
                 testAcc = 0
                 for fl in range(0,folK):
+                    #instantiate SVM everytime
+                    clf = svm.OneClassSVM(nu=.01)
                     rN = np.random.choice(frames, frames, replace=False) 
                     
                     X_train = allActs['Figure_' + stim[ee][sTR]][rN[0:int(frames/2)],:]
                     
                     clf.fit(X_train)
                     tempAcc_train = clf.predict(X_train)
-                    trainAcc = trainAcc + ((frames/2) - tempAcc_train[tempAcc_train == -1].size)/(frames/2)
+                    trainAcc = ((frames/2) - tempAcc_train[tempAcc_train == -1].size)/(frames/2)
                 
                     #Test on object, but left out frames
                     X_test = allActs['Figure_' + stim[ee][sTE]][rN[int(frames/2):frames],:]
                     tempAcc_test = clf.predict(X_test)
-                    testAcc = testAcc + ((frames/2) - tempAcc_test[tempAcc_test == -1].size)/(frames/2)
+                    testAcc = ((frames/2) - tempAcc_test[tempAcc_test == -1].size)/(frames/2)
                     
-                CNN_Acc[n,0] = exp[ee]
-                CNN_Acc[n,1] = modelType[mm]
-                CNN_Acc[n,2] = 'Figure_' + stim[ee][sTR]
-                CNN_Acc[n,3] = 'Figure_' + stim[ee][sTE]
-                
-                #Check if first 2 characters are same 
-                #to determine whether skel is the same
-                
-                if exp[ee] == 'Exp1':
-                        
-                    if stim[ee][sTR][0:2] == stim[ee][sTE][0:2]:
-                        skel = 'Same'
-                    else:
-                        skel = 'Diff'
+                    CNN_Acc[n,0] = exp[ee]
+                    CNN_Acc[n,1] = modelType[mm]
+                    CNN_Acc[n,2] = 'Figure_' + stim[ee][sTR]
+                    CNN_Acc[n,3] = 'Figure_' + stim[ee][sTE]
                     
-                        
-                elif exp[ee] == 'Exp2':
-                    if stim[ee][sTR][4:5] == stim[ee][sTE][4:5]:
-                        skel = 'Same'
-                    else:
-                        skel = 'Diff'
+                    #Check if first 2 characters are same 
+                    #to determine whether skel is the same
                     
-                #check if surface forms are the same
-                if stim[ee][sTR][-4:] == stim[ee][sTE][-4:]:
-                    SF = 'Same'
-                else:
-                    SF = 'Diff'
+                    if exp[ee] == 'Exp1':
                             
-                CNN_Acc[n,4] = skel
-                CNN_Acc[n,5] = SF
-                CNN_Acc[n,6] = trainAcc/folK
-                CNN_Acc[n,7] = testAcc/folK
-                
-                print(exp[ee], modelType[mm], skel, SF, CNN_Acc[n,6], CNN_Acc[n,7])
-                
-                n = n +1
+                        if stim[ee][sTR][0:2] == stim[ee][sTE][0:2]:
+                            skel = 'Same'
+                        else:
+                            skel = 'Diff'
+                        
+                            
+                    elif exp[ee] == 'Exp2':
+                        if stim[ee][sTR][4:5] == stim[ee][sTE][4:5]:
+                            skel = 'Same'
+                        else:
+                            skel = 'Diff'
+                        
+                    #check if surface forms are the same
+                    if stim[ee][sTR][-4:] == stim[ee][sTE][-4:]:
+                        SF = 'Same'
+                    else:
+                        SF = 'Diff'
+                                
+                    CNN_Acc[n,4] = skel
+                    CNN_Acc[n,5] = SF
+                    CNN_Acc[n,6] = trainAcc
+                    CNN_Acc[n,7] = testAcc
+                    
+                    print(exp[ee], modelType[mm], skel, SF, CNN_Acc[n,6], CNN_Acc[n,7])
+                    
+                    n = n +1
                 
                 
                 
--- LiMA_Model_SVM_Size.py
+++ LiMA_Model_SVM_Size.py
@@ -25,7 +25,7 @@ exp = ['Exp1', 'Exp2']
 
 stim = [['23_Skel', '23_Bulge', '31_Skel', '31_Bulge','26_Skel', '26_Bulge'], \
         ['31_0_Skel', '31_0_Bulge','31_50_Skel', '31_50_Bulge']]
-modelType = ['FF_SN', 'R_SN']
+modelType = ['R_SN']
 IMsize = str(25)
 
 
@@ -33,16 +33,16 @@ frames= 300
 labels = [np.repeat(1, frames).tolist(), np.repeat(2, frames).tolist()]
 #labels = list(chain(*labels))
 
-folK = 5
+folK = 10
 
 #For single class SVM
 #Nu value is the proportion of outliers you expect (i.e., upper-bound on training data)
 #Gamma parameter determines smoothing of the edges of data (i.e., the )
-clf = svm.OneClassSVM(nu=.01)
+
 
 for ee in range(0,len(exp)):
     n = 0
-    CNN_Acc = np.empty((len(stim[ee]) * len(stim[ee])*len(modelType),8), dtype = object)
+    CNN_Acc = np.empty(((len(stim[ee]) * len(stim[ee])*len(modelType))*10,8), dtype = object)
     
     for mm in range(0, len(modelType)):      
         
@@ -54,56 +54,59 @@ for ee in range(0,len(exp)):
                 trainAcc = 0
                 testAcc = 0
                 for fl in range(0,folK):
+                    #instantiate SVM everytime
+                    clf = svm.OneClassSVM(nu=.01)
+                    
                     rN = np.random.choice(frames, frames, replace=False) 
                     
                     X_train = allActs['Figure_' + stim[ee][sTR]][rN[0:int(frames/2)],:]
                     
                     clf.fit(X_train)
                     tempAcc_train = clf.predict(X_train)
-                    trainAcc = trainAcc + ((frames/2) - tempAcc_train[tempAcc_train == -1].size)/(frames/2)
+                    trainAcc = ((frames/2) - tempAcc_train[tempAcc_train == -1].size)/(frames/2)
                 
                     #Test on object, but left out frames
                     X_test = allActsSize['Figure_' + stim[ee][sTE]][rN[int(frames/2):frames],:]
                     tempAcc_test = clf.predict(X_test)
-                    testAcc = testAcc + ((frames/2) - tempAcc_test[tempAcc_test == -1].size)/(frames/2)
+                    testAcc = ((frames/2) - tempAcc_test[tempAcc_test == -1].size)/(frames/2)
                     
-                CNN_Acc[n,0] = exp[ee]
-                CNN_Acc[n,1] = modelType[mm]
-                CNN_Acc[n,2] = 'Figure_' + stim[ee][sTR]
-                CNN_Acc[n,3] = 'Figure_' + stim[ee][sTE]
-                
-                #Check if first 2 characters are same 
-                #to determine whether skel is the same
-                
-                if exp[ee] == 'Exp1':
-                        
-                    if stim[ee][sTR][0:2] == stim[ee][sTE][0:2]:
-                        skel = 'Same'
-                    else:
-                        skel = 'Diff'
+                    CNN_Acc[n,0] = exp[ee]
+                    CNN_Acc[n,1] = modelType[mm]
+                    CNN_Acc[n,2] = 'Figure_' + stim[ee][sTR]
+                    CNN_Acc[n,3] = 'Figure_' + stim[ee][sTE]
                     
-                        
-                elif exp[ee] == 'Exp2':
-                    if stim[ee][sTR][4:5] == stim[ee][sTE][4:5]:
-                        skel = 'Same'
-                    else:
-                        skel = 'Diff'
+                    #Check if first 2 characters are same 
+                    #to determine whether skel is the same
                     
-                #check if surface forms are the same
-                if stim[ee][sTR][-4:] == stim[ee][sTE][-4:]:
-                    SF = 'Same'
-                else:
-                    SF = 'Diff'
+                    if exp[ee] == 'Exp1':
                             
-                CNN_Acc[n,4] = skel
-                CNN_Acc[n,5] = SF
-                CNN_Acc[n,6] = trainAcc/folK
-                CNN_Acc[n,7] = testAcc/folK
-                
-                print(exp[ee], modelType[mm], skel, SF, CNN_Acc[n,6], CNN_Acc[n,7])
-                
-                n = n +1
-             
+                        if stim[ee][sTR][0:2] == stim[ee][sTE][0:2]:
+                            skel = 'Same'
+                        else:
+                            skel = 'Diff'
+                        
+                            
+                    elif exp[ee] == 'Exp2':
+                        if stim[ee][sTR][4:5] == stim[ee][sTE][4:5]:
+                            skel = 'Same'
+                        else:
+                            skel = 'Diff'
+                        
+                    #check if surface forms are the same
+                    if stim[ee][sTR][-4:] == stim[ee][sTE][-4:]:
+                        SF = 'Same'
+                    else:
+                        SF = 'Diff'
+                                
+                    CNN_Acc[n,4] = skel
+                    CNN_Acc[n,5] = SF
+                    CNN_Acc[n,6] = trainAcc
+                    CNN_Acc[n,7] = testAcc
+                    
+                    print(exp[ee], modelType[mm], skel, SF, CNN_Acc[n,6], CNN_Acc[n,7])
+                    
+                    n = n +1
+                 
                 
         np.savetxt('Results/LiMA_' + exp[ee] + '_allModels_OneClassSVM_Size.csv', CNN_Acc, delimiter=',', fmt= '%s')
             
