1ba7d3846af8665ba78d12f869d007f97b8c8826
vayzenb <vayzenberg90@gmail.com>
2020-03-17 15:48:52 -0400

    GBJ acts

--- LiMA_GBJ_Acts.m
+++ LiMA_GBJ_Acts.m
@@ -8,10 +8,11 @@ stim = {{'23_Skel', '23_Bulge', '31_Skel', '31_Bulge','26_Skel', '26_Bulge'}, {'
 skel = {{'23','31', '26'},{'31_0', '31_50'}};
 SF = {'Skel', 'Bulge'};
 
-imScale = .50;
-frames = 301; %Number of frames
+imScale = .2;
+imTrans = .2;
+frames = 300; %Number of frames
 
-rF = randperm(frames,20);
+%rF = randperm(frames,20);
 
 
 for ee = 1:length(exp)
@@ -24,13 +25,21 @@ for ee = 1:length(exp)
             sizeActs = [];
 			
             st = 1;
-			for fn = 1:length(rF)
+			for fn = 1:frames
 				%Load original image
 				ogIM = imread(['Frames/Figure_', skel{ee}{sk},'_',SF{sf}, '_', int2str(fn), '.jpg']);
-				padSize = round(((size(ogIM,1)*(imScale+1))- size(ogIM,1))/2,0);
+                sizeIM = zeros(round(size(ogIM,1)*(imScale+1)), round(size(ogIM,1)*(imScale+1)),3,'uint8');
+                sizeIM(:,:,1) = 119;
+                sizeIM(:,:,2) = 119;
+                sizeIM(:,:,3) = 119;
+                %Overlay OG image on blank in top left corner
+                %This reduces the size and shifts it by imScale %
+                sizeIM(1:size(ogIM,1), 1:size(ogIM,1), :) = ogIM;
+                
+                %padSize = round(((size(ogIM,1)*(imScale+1))- size(ogIM,1))/2,0);
 
 				%Resize images by 25%
-				sizeIM = padarray(ogIM, [padSize, padSize], 119); %Pad with 119 (gray) by % size reduction
+				%sizeIM = padarray(ogIM, [0, padSize*2], 119); %Pad with 119 (gray) by % size reduction
 				
 				%Load diff SF image
 				if strcmp(SF{sf},'Skel')
@@ -59,8 +68,8 @@ for ee = 1:length(exp)
 				sizeActs(st,:) = sizeGBJ;
                 
 				%Calculate diff
-				sizeDiff = mean(abs(ogGBJ - sizeGBJ));
-				sfDiff = mean(abs(ogGBJ - sfGBJ));
+				sizeDiff = norm(ogGBJ - sizeGBJ);
+				sfDiff = norm(ogGBJ - sfGBJ);
 
 
 
@@ -72,14 +81,15 @@ for ee = 1:length(exp)
                 allData{n,5} = sizeDiff;
 
                 n= n +1;
+                st = st+1;
 
 			end
 			%Save GBJ acts as file
 			save(['GBJ_Acts/Figure_', skel{ee}{sk},'_',SF{sf}, '_GBJActs'], 'stimActs');
 			save(['GBJ_Acts/Figure_', skel{ee}{sk},'_',SF{sf}, '_GBJActs_Size', int2str(imScale*100)], 'sizeActs');
-            stim{ee}{st}
-            st = st+1;
-		end
+                
+        end
+        sk
 	end
 	allData = cell2table(allData, 'VariableNames', {'Skel', 'SF', 'Frame', 'sfDiff', 'sizeDiff'});
 	writetable(allData, ['Results/LiMA_', exp{ee}, '_GBJ_diffs_',  int2str(imScale*100), '.csv']);
