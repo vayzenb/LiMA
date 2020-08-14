500571ca575ec9be9e00157370654a09ff139115
vayzenb <vayzenberg90@gmail.com>
2020-08-14 01:13:21 -0400

    updated file names

--- LiMA_OSL_SVM.py
+++ LiMA_OSL_SVM.py
@@ -27,9 +27,9 @@ modelType = ['FF_SN','R_SN', 'FF_IN', 'R_IN', 'GBJ', 'GIST']
 
 
 total_frames= 300
-train_frames = 300
+train_frames = 150
 test_frames = total_frames - train_frames
-test_frames = 300
+
 
 folK = 10
 
